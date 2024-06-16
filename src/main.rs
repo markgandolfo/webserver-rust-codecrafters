dda705e8188aac59c31b9e1ded43e0ba1ecee9b4
Mark Gandolfo <mark@gandolfo.com.au>
2024-06-16 22:25:44 +1000

    add some simple routing with 404s

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,43 @@
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+
+#[derive(Debug)]
+struct Request {
+    method: String,
+    target: String,
+    version: String,
+}
+
+impl Request {
+    fn from_string(request: &str) -> Request {
+        let mut parts = request.split_whitespace();
+        Request {
+            method: parts.next().unwrap().to_string(),
+            target: parts.next().unwrap().to_string(),
+            version: parts.next().unwrap().to_string(),
+        }
+    }
+}
+
+fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
+    let mut buf = [0; 1024];
+    stream.read(&mut buf)?;
+
+    let request = String::from_utf8_lossy(&buf);
+    let request = Request::from_string(&request);
+
+    match request.target.as_str() {
+        "/" => {
+            stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
+            stream.write(b"Hello, World!")?;
+        }
+        _ => {
+            stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?;
+        }
+    }
+
+    Ok(())
+}
 
 fn main() {
     println!("Logs from your program will appear here!");
@@ -9,11 +47,12 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                println!("accepted new connection");
-                _stream.write(b"HTTP/1.1 200 OK\r\n\r\n").expect("200 \n");
+                if let Err(e) = handle_client(_stream) {
+                    eprintln!("failed to handle client: {}", e);
+                }
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("failed to accept client: {}", e);
             }
         }
     }
