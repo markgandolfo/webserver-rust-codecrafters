a3b4e359cee57cb51c5269e8f9480e338808bf47
Mark Gandolfo <mark@gandolfo.com.au>
2024-06-17 18:25:29 +1000

    echo the user agent

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,24 @@ struct Request {
     method: String,
     target: String,
     version: String,
+    user_agent: String,
 }
 
 impl Request {
     fn from_string(request: &str) -> Request {
-        let mut parts = request.split_whitespace();
+        let header_lines: Vec<_> = request.split("\n").collect();
+        let user_agent: Vec<_> = header_lines[2].split(":").collect();
+        let mut user_agent_string = String::from("");
+        if user_agent.len() == 2 {
+            user_agent_string = user_agent[1].trim().to_string();
+        }
+
+        let mut parts = header_lines[0].split_whitespace();
         Request {
             method: parts.next().unwrap().to_string(),
             target: parts.next().unwrap().to_string(),
             version: parts.next().unwrap().to_string(),
+            user_agent: user_agent_string,
         }
     }
 }
@@ -25,17 +34,33 @@ fn handle_client(mut stream: TcpStream) -> std::io::Result<()> {
 
     let request = String::from_utf8_lossy(&buf);
     let request = Request::from_string(&request);
-    println!("req: {}", request.target.as_str());
 
     match request.target.as_str() {
         "/" => {
             stream.write(b"HTTP/1.1 200 OK\r\n\r\n")?;
             stream.write(b"Hello, World!")?;
         }
+        "/user-agent" => {
+            println!("user agent: {}", request.user_agent);
+            stream.write(
+                format!(
+                    "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                    request.user_agent.len(),
+                    request.user_agent
+                )
+                .as_bytes(),
+            )?;
+        }
         path if path.starts_with("/echo/") => {
-            println!("Here");
             if let Some(text) = path.strip_prefix("/echo/") {
-                stream.write(format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", text.len(), text).as_bytes())?;
+                stream.write(
+                    format!(
+                        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                        text.len(),
+                        text
+                    )
+                    .as_bytes()
+                )?;
             } else {
                 stream.write(b"HTTP/1.1 404 Not Found\r\n\r\n")?;
             }
